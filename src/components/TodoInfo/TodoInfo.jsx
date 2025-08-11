3e98f191b456bf29972ec19174bff4f8d43e6171
Dina Zhyliaieva <din.zhyliaieva@gmail.com>
2025-08-11 15:46:35 +0300

    TodoInfo TodoInfo--

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,19 @@
+
 // Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+export const TodoInfo = ({ todo }) => {
+  return (
+    <article
+      className=
+        {todo.completed
+          ? 'TodoInfo TodoInfo--completed'
+          : 'TodoInfo TodoInfo--active'}>
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+      <a
+        className="UserInfo"
+        href={todo.user.email ? `mailto:${todo.user.email}` : '#'}
+      >
+        {todo.user.name}
+      </a>
+    </article>
+  );
+};
